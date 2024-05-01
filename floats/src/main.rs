0f9c7a0ce92b2f7a28db8af02eb357942dd0eafe
mywyau <47248104+mywyau@users.noreply.github.com>
2024-05-01 12:33:09 +0100

    floats adding notes on data types

--- floats/src/main.rs
+++ floats/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 2.0;   // two primitive types
+    let x: f32 = 3.0;
+
+    // Rust supports the basic mathematical operations you’d expect for all the number types:
+    // addition, subtraction, multiplication, division, and remainder.
+
+    // Integer division truncates toward zero to the nearest integer.
+
+    let sum = 5 + 10;
+    let difference = 95.5 + 4.3;
+    let product = 4 * 30;
+    let quotient = 56.7 * 32.2;
+    let truncated = -5 / 3;
+
+    let remainder = 43 % 5;
+
+    println!("sum: {}", sum);
+    println!("difference: {}", difference);
+    println!("product: {}", product);
+    println!("quotient: {}", quotient);
+    println!("truncated: {}", truncated);
+    println!("remainder: {}", remainder);
 }
+
+
---
+++ floats/target/debug/deps/floats-c9699014d3619f74.d
@@ -0,0 +1,5 @@
+/Users/michaelyau/rust-notes/floats/target/debug/deps/floats-c9699014d3619f74: src/main.rs
+
+/Users/michaelyau/rust-notes/floats/target/debug/deps/floats-c9699014d3619f74.d: src/main.rs
+
+src/main.rs:
