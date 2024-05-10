679cc3d45f25f9fd63c1bf8ec6a8a823055386fe
mywyau <47248104+mywyau@users.noreply.github.com>
2024-05-10 13:58:32 +0100

    more notes on methods and structs/impl

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -4,11 +4,25 @@ struct Rectangle {
     height: u32,
 }
 
+// Each struct is allowed to have multiple impl blocks. For example, Listing 5-15 is equivalent to the code shown in Listing 5-16, which has each method in its own impl block.
+// There’s no reason to separate these methods into multiple impl blocks here, but this is valid syntax.
+// We’ll see a case in which multiple impl blocks are useful for generic types and traits.
+
 impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
 
+    // Associated_functions
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
@@ -39,4 +53,14 @@ fn main() {
 
     println!("Can rectange1 hold rectangle2? {}", rectangle1.can_hold(&rectangle2));
     println!("Can rectange1 hold rectangle3? {}", rectangle1.can_hold(&rectangle3));
+
+    // Associated functions example
+    let square = Rectangle::square(3);
+
+    println!(
+        "The area of the square is {} square pixels.",
+        square.area()
+    );
 }
+
+
